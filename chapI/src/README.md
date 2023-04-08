650156356863cdc0705495f9f745d8467ee899e2
talisma <talismacassoma.manuel@uit.ac.ma>
2023-04-08 00:16:49 +0000

    verify and post deposit

---
+++ chapI/src/README.md
@@ -0,0 +1,23 @@
+
+# requisitos
+
+- [x] deve ser possivel criar uma conta
+- [] deve ser possivel buscar o extrato bancario
+- [] deve ser possivel realizar um deposito
+- [] deve ser possivel realizar um saque
+- [] deve ser possivel buscar o extrato bancario do cliente por data
+- [] deve ser possivel actulizar dados da conta do cliente 
+- [] deve ser possivel obter dados da conta do cliente
+- [] deve ser possivel deletar uma conta 
+
+
+# regras de negocio
+
+- [] nao deve ser possivel criar uma conta ja existente
+- [] nao deve ser possivel realizar um deposito de conta inexistente
+- [] nao deve ser possivel buscar extrato de conta inexistente
+- [] nao deve ser possivel ealizar um saque de conta inexistente
+- [] nao deve ser possivel actulizar dados da conta do cliente
+- [] nao deve ser possivel obter dados da conta do cliente
+- [] nao deve ser possivel deletar uma conta inexistente
+- [] nao deve ser possivel realizar um saque qd o saldo é insuficiente
--- chapI/src/index.js
+++ chapI/src/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const {v4: uuidv4}= require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 const customers = [];
@@ -8,23 +8,71 @@ const customers = [];
 //middleware
 app.use(express.json());
 
-//create a account route
-app.post('/account',(request, response)=>{
+//verify 
+function verifyIfExisteCPF(request, response, next){
 
-   const {cpf, name}= request.body;
+   const{ cpf } = request.headers;
+   const customer = customers.find((customer)=> customer.cpf === cpf);
+   
+   if(!customer){
+      return response.status(400).send("account not found!")
+   }
+
+   request.customer = customer
+   //console.log(request.customer)
+
+   return next();
+
+}
+//create a account route
+app.post('/account', (request, response) => {
+
+   const { cpf, name } = request.body;
+
+   const isAlreadyCreated = customers.some((customer) => customer.cpf === cpf);
+
+   if (isAlreadyCreated) {
+      return response.status(400).send("account already exists!");
+   }
 
    customers.push({
       id: uuidv4(),
       name,
-      cpf,  
+      cpf,
       statement: []
    });
 
    console.log("account:", customers);
-   return response.satus(201).send("account created sucessfully!");
+   return response.status(201).send("account created sucessfully!");
+
 });
 
+//show statement 
+app.get("/statement", verifyIfExisteCPF, (request, resposne)=>{
+const { customer } = request;
+
+   return resposne.json(customer.statement);
+})
+
+//deposit
+app.post("/deposit", verifyIfExisteCPF, (request, response)=>{
+const { description, amount} = request.body;
+const {customer} = request;
+
+const statementOperation ={
+   description,
+   amount,
+   created_at: new Date(),
+   type: "deposit"
+};
+
+customer.statement.push(statementOperation);
+
+return response.status(201).send();
+
+})
+
 //PORTA
-app.listen(3333, ()=>{
-    console.log('server started XD');
+app.listen(3333, () => {
+   console.log('server started XD');
 });
